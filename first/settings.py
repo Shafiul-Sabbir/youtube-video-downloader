0cac2645fdb5914f2e412af9f7ceb970410d133c
Shafiul-Sabbir <sabbirvai82@gmail.com>
2024-02-24 01:44:01 +0600

    changes

--- first/settings.py
+++ first/settings.py
@@ -126,5 +126,5 @@ STATICFILES_DIRS = [STATIC_DIR,]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 # STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
