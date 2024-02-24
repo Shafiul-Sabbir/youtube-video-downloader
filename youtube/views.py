9ac11e91b264a9c81ee6e5d97b6b33cea8f594a1
Shafiul-Sabbir <sabbirvai82@gmail.com>
2024-02-24 22:13:39 +0600

    changes

--- youtube/views.py
+++ youtube/views.py
@@ -15,7 +15,10 @@ def youtube(request):
         stream = video.streams.get_lowest_resolution()
         
         # specify the path where you I to save the video
-        download_folder = 'C:\\Downloads\\'
+        # download_folder = 'C:\\Downloads\\'
+        # Modify the download_folder to the default downloads directory
+        download_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+
         
         # create the folder if it doesnt exist
         if not os.path.exists(download_folder):
@@ -31,3 +34,47 @@ def youtube(request):
 
 def successful_page(request):
     return render(request, '../templates/youtube/successful.html')
+
+# from django.shortcuts import render, redirect
+# from pytube import YouTube
+# import tempfile
+# import os
+
+# # Create your views here.
+
+# def youtube(request):
+#     if request.method == "POST":
+#         link = request.POST.get('link')
+        
+#         if not link:
+#             return render(request, 'youtube/youtube.html', {'error': 'Please provide a valid YouTube link'})
+
+#         try:
+#             video = YouTube(link)
+#             stream = video.streams.get_lowest_resolution()
+#         except Exception as e:
+#             return render(request, 'youtube/youtube.html', {'error': f'Error processing YouTube video: {str(e)}'})
+
+#         # Use a temporary directory for file downloads
+#         with tempfile.TemporaryDirectory() as temp_dir:
+#             try:
+#                 # Downloads video to the temporary directory
+#                 stream.download(temp_dir)
+                
+#                 # Specify the relative path where you want to save the file in your static directory
+#                 relative_path = f'videos/{video.title}.mp4'
+                
+#                 # Assuming your static directory is configured correctly in settings.py
+#                 file_path = os.path.join('static', relative_path)
+                
+#                 # Move the downloaded file to the static directory
+#                 os.rename(os.path.join(temp_dir, f'{video.title}.mp4'), file_path)
+                
+#                 return render(request, 'youtube/successful.html', {'file_path': file_path})
+#             except Exception as e:
+#                 return render(request, 'youtube/youtube.html', {'error': f'Error saving the video: {str(e)}'})
+
+#     return render(request, 'youtube/youtube.html')
+
+# def successful_page(request):
+#     return render(request, 'youtube/successful.html')
